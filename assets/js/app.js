6b0f2e37516f7b57400c094b436dcb7e404d4acf
abdulahad <124651992+AbdullAhad123@users.noreply.github.com>
2024-11-10 15:34:44 +0500

    Update UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,10 @@
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove", (e) => {
-  cursor.setAttribute(
-    "style",
-    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
-  );
-}),
-  document.addEventListener("click", (e) => {
-    cursor.classList.add("expand"),
-      setTimeout(() => {
-        cursor.classList.remove("expand");
-      }, 500);
-  });
+$(".featuredProjectBanner").on("mouseover mouseenter", function () {
+  const image = $(this).find("img");
+  const image_height = image.height();
+  image.css("transform", "translateY(-" + (image_height - 450) + "px)");
+});
 
-function toggleMouseIcon() {
-  const scrolled = $(window).scrollTop();
-  if (scrolled >= 100) {
-    $(".scrollDownMouse").fadeOut();
-  } else {
-    $(".scrollDownMouse").fadeIn();
-  }
-}
-window.addEventListener("scroll", toggleMouseIcon);
+$(".featuredProjectBanner").on("mouseleave", function () {
+  const image = $(this).find("img");
+  image.css("transform", "translateY(0)");
+});
